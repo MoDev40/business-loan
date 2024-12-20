4569a9926e55da5309268124a6364dab1692e53f
M40 <modev.404@gmail.com>
2024-12-20 10:31:50 +0300

    create login functionality

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function login()
+    {
+        return view('auth.index');
+    }
+    public function authenticate(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            // return redirect()->route('dashboard.index')->with('success', 'Login successfully');
+        }
+    }
+}

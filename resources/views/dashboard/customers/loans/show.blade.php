b7362eb3e5ebe191fa57dc91e5fa09664223a95e
M40 <modev.404@gmail.com>
2024-12-26 13:53:37 +0300

    display customer loan info

--- resources/views/dashboard/customers/loans/show.blade.php
+++ resources/views/dashboard/customers/loans/show.blade.php
@@ -28,14 +28,46 @@
 </nav> 
 
 <div class="mt-8">
-    <h1>Loading....</h1>
+    <div class="grid grid-cols-2 gap-6">
+        <div>
+            <Label class="font-semibold">Name</Label>
+            <p class="font-light text-gray-500">{{$data->customer->name}}</p>
+        </div>
+        <div>
+            <Label class="font-semibold">Email</Label>
+            <p class="font-light text-gray-500">{{$data->customer->email}}</p>
+        </div>
+        <div>
+            <Label class="font-semibold">Phone</Label>
+            <p class="font-light text-gray-500">{{$data->customer->phone}}</p>
+        </div>
+        <div>
+            <Label class="font-semibold">Address</Label>
+            <p class="font-light text-gray-500">{{$data->customer->address}}</p>
+        </div>
+    </div>
+        <hr class="mt-8">
+    <div class="grid grid-cols-3 gap-4 mt-4">
+        <div>
+            <Label class="font-semibold">Paid</Label>
+            <p class="text-green-600">${{$totalPayed}}</p>
+        </div>
+        <div>
+            <Label class="font-semibold">Remaining</Label>
+            <p class="text-red-600">${{$data->amount-$totalPayed}}</p>
+        </div>
+        <div>
+            <Label class="font-semibold">Total Loan</Label>
+            <p>${{$data->amount}}</p>
+        </div>
+    </div>
 </div>
 
 <button 
     type="button" 
     data-modal-target="default-modal" 
     data-modal-toggle="default-modal" 
-    class="text-white mt-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+    class="text-white mt-10 w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
 >
 Pay
 </button>
